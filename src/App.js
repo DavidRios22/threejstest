8efc4c723fea814c62f0ee42e2fc359b0b4f7bdd
DavidRios22 <davidriosdoeswork@gmail.com>
2023-10-16 16:46:07 -0500

    added loading animation and started continue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import "./App.css"
+import { useState, useEffect } from "react"
 import { Canvas } from "@react-three/fiber"
 import { AsciiRenderer} from "@react-three/drei"
 import Cube from "./Cube"
 
 function App() {
-  const loadingBar = ""
+
+  const [pipeColors, setPipeColors] = useState(Array(10).fill('black'));
+
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      const revealPipes = () => {
+        setPipeColors((prevColors) => {
+          const updatedColors = [...prevColors];
+          const nextIndex = updatedColors.findIndex((color) => color === 'black');
+          if (nextIndex >= 0) {
+            updatedColors[nextIndex] = 'white';
+          }
+          return updatedColors;
+        })
+      };
+
+      const intervalId = setInterval(revealPipes, 200);
+
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, 1000);
+
+    return () => {
+      clearTimeout(delay);
+    };
+  }, []);
+
+  
+
+
+
+  
   return (
     <div className="box">
       <Canvas>
@@ -13,8 +46,13 @@ function App() {
         <Cube />
       </Canvas>
       <div className="loading">
-        <p>{loadingBar}</p>
+        {pipeColors.map((color, index) => (
+          <span key={index} style={{ color }}>
+            {"/"}
+          </span>
+        ))}
       </div>
+      <p className="continue">continue</p>
     </div>
   )
 }
